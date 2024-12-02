a3f4dd47406f2167682ad6fdb3b0678083888229
Esther Valerio Tinetti <esther.tinetti@dev.auaha.com.br>
2024-12-01 22:02:52 -0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,36 @@
+
+const button = document.getElementById('button-calc');
+
+let xpHeroi;
+let nivelHeroi;
+
+button.onclick = function () {
+    xpHeroi = document.getElementById('input-xp').value; 
+    if (xpHeroi === '') {
+        alert('Insira um valor');
+    } else {
+        xpHeroi = parseInt(xpHeroi, 10); // Converte o valor para número
+        if (xpHeroi <= 1000) {
+            nivelHeroi = "Ferro";
+        } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+            nivelHeroi = "Bronze";
+        } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+            nivelHeroi = "Prata";
+        } else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+            nivelHeroi = "Ouro";
+        } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+            nivelHeroi = "Platina";
+        } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+            nivelHeroi = "Ascendente";
+        } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+            nivelHeroi = "Imortal";
+        } else {
+            nivelHeroi = "Radiante";
+        }
+
+        // Exibe o resultado na div com a classe "result-xp"
+        document.querySelector('.result-xp').innerHTML = `Nível do Herói: <strong>${nivelHeroi}</strong>`;
+    }
+    
+};
+
